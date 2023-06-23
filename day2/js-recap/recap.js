0a7c1f7f17563c0e0d4d7cc52ee6c975506ab46d
maaik <info>
2023-06-23 21:01:49 +0200

    js recap

---
+++ day2/js-recap/recap.js
@@ -0,0 +1,22 @@
+// comments in JS with //
+/* or multline
+with */
+
+// variables
+let name = "Maaike";
+const PI = 3.14;
+
+// function
+function doSomething(x, y) {
+    return x + y;
+}
+
+// arrow functions
+let x = (name) => "hello" + name;
+x("Hugh");
+
+// equivalent of
+function sayHi(name) {
+    return "hello" + name;
+}
+
