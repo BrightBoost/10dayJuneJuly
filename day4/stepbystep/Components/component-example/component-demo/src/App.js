61928a6a9d11223f0efcc2df3f35e4e50ac81d4a
maaik <info>
2023-06-27 16:49:48 +0200

    components

--- day4/stepbystep/Components/component-example/component-demo/src/App.js
+++ day4/stepbystep/Components/component-example/component-demo/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/SearchBar';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <SearchBar />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ day4/stepbystep/Components/component-example/component-demo/src/components/SearchBar.js
@@ -0,0 +1,10 @@
+function SearchBar() {
+    return (
+        <div>
+            <input type="text" placeholder="search" />
+            <button>Click me</button>
+        </div>
+    )
+}
+
+export default SearchBar;
